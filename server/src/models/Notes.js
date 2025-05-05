f25574b13d4967681cc6e11cb8d02c6d429c1af3
Bhuvanesh-23 <bhuvaneshm.ai@gmail.com>
2025-05-06 04:33:14 +0530

    MVC architecture completed for the backend

--- server/src/models/Notes.js
+++ server/src/models/Notes.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    content: { type: String },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    folder: { type: mongoose.Schema.Types.ObjectId, ref: 'Folder' },
-    tags: [String],
+    content: { type: mongoose.Schema.Types.ObjectId,ref:"content.model", required: true },
+    tags: {type: [String], default: []},
     isPinned: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
---
+++ server/src/services/content.service.js
@@ -0,0 +1,22 @@
+const ContentModel = require('../models/content.model');
+
+class ContentService {
+    async addContent(text, isPrivate=false) {
+        await ContentModel.create({
+            text,
+            isPrivate
+        });
+        M
+    }
+    async getPrivateContents() {
+        const contents = await ContentModel.find({ isPrivate : true });
+        return contents;
+    }
+    async getPublicContents() {
+        const contents = await ContentModel.find({ isPrivate : false });
+        return contents;
+    }
+}
+
+const contentService = new ContentService();
+module.exports = contentService;
